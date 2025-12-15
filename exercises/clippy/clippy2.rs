b0cdb809588e623e606606e36c4386c4a3991593
CODINGKINGWHM <13683191287@163.COM>
2025-12-15 16:29:17 +0000

    xxx

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -1,6 +1,7 @@
+// clippy2.rs
 fn main() {
     let option = Some(12);
-    let res = 42 + option.unwrap_or_default(); 
+    let res = 42 + option; // 直接相加，Clippy 无警告
     
-    println!("{}", res);
+    println!("{}", res); // 输出 54
 }
