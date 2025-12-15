2aa8d77ffd33e1471a5573c3774197ea5bbd0b16
CODINGKINGWHM <13683191287@163.COM>
2025-12-15 15:20:41 +0000

    xxx

--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -1,32 +1,27 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Queue<T> {
     elements: Vec<T>,
 }
 
 impl<T> Queue<T> {
-    pub fn new() -> Queue<T> {
-        Queue {
-            elements: Vec::new(),
-        }
+    pub fn new() -> Self {
+        Queue { elements: Vec::new() }
     }
 
     pub fn enqueue(&mut self, value: T) {
-        self.elements.push(value)
+        self.elements.push(value);
     }
 
-    pub fn dequeue(&mut self) -> Result<T, &str> {
-        if !self.elements.is_empty() {
-            Ok(self.elements.remove(0usize))
-        } else {
-            Err("Queue is empty")
-        }
+    pub fn dequeue(&mut self) -> Result<T, &'static str> {
+        self.elements
+            .remove(0)
+            .ok_or("Queue is empty")
     }
 
-    pub fn peek(&self) -> Result<&T, &str> {
-        match self.elements.first() {
-            Some(value) => Ok(value),
-            None => Err("Queue is empty"),
-        }
+    pub fn peek(&self) -> Result<&T, &'static str> {
+        self.elements
+            .first()
+            .ok_or("Queue is empty")
     }
 
     pub fn size(&self) -> usize {
@@ -39,64 +34,76 @@ impl<T> Queue<T> {
 }
 
 impl<T> Default for Queue<T> {
-    fn default() -> Queue<T> {
-        Queue {
-            elements: Vec::new(),
-        }
+    fn default() -> Self {
+        Self::new()
     }
 }
 
-pub struct myStack<T>
-{
-	q1:Queue<T>,
-	q2:Queue<T>
+#[derive(Debug, Clone)]
+pub struct MyStack<T> {
+    q1: Queue<T>,
+    q2: Queue<T>,
 }
 
-impl<T> myStack<T> {
+impl<T> MyStack<T> {
     pub fn new() -> Self {
         Self {
-			q1:Queue::<T>::new(),
-			q2:Queue::<T>::new()
+            q1: Queue::new(),
+            q2: Queue::new(),
         }
     }
 
     pub fn push(&mut self, elem: T) {
         self.q2.enqueue(elem);
-        while !self.q1.is_empty() {
-            self.q2.enqueue(self.q1.dequeue().unwrap());
+        while let Ok(val) = self.q1.dequeue() {
+            self.q2.enqueue(val);
         }
         std::mem::swap(&mut self.q1, &mut self.q2);
     }
 
-    pub fn pop(&mut self) -> Result<T, &str> {
-        self.q1.dequeue()
+    pub fn pop(&mut self) -> Result<T, &'static str> {
+        self.q1.dequeue().map_err(|_| "Stack is empty")
     }
 
     pub fn is_empty(&self) -> bool {
         self.q1.is_empty()
     }
+
+    pub fn size(&self) -> usize {
+        self.q1.size()
+    }
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
-	
-	#[test]
-	fn test_queue(){
-		let mut s = myStack::<i32>::new();
-		assert_eq!(s.pop(), Err("Stack is empty"));
+    use super::*;
+
+    #[test]
+    fn test_my_stack() {
+        let mut s = MyStack::<i32>::new();
+        
+        assert_eq!(s.pop(), Err("Stack is empty"));
+        
         s.push(1);
         s.push(2);
         s.push(3);
+        assert_eq!(s.size(), 3);
+        
         assert_eq!(s.pop(), Ok(3));
         assert_eq!(s.pop(), Ok(2));
+        assert_eq!(s.size(), 1);
+        
         s.push(4);
         s.push(5);
         assert_eq!(s.is_empty(), false);
+        assert_eq!(s.size(), 3);
+        
         assert_eq!(s.pop(), Ok(5));
         assert_eq!(s.pop(), Ok(4));
         assert_eq!(s.pop(), Ok(1));
+        
         assert_eq!(s.pop(), Err("Stack is empty"));
         assert_eq!(s.is_empty(), true);
-	}
+        assert_eq!(s.size(), 0);
+    }
 }
