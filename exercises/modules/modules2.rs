39188c91c661a02474246ef20da73e32aab9ab3c
CODINGKINGWHM <13683191287@163.COM>
2025-12-15 15:43:34 +0000

    xxx

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -1,18 +1,7 @@
-// modules2.rs
-//
-// You can bring module paths into scopes and provide new names for them with
-// the 'use' and 'as' keywords. Fix these 'use' statements to make the code
-// compile.
-//
-// Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
 mod delicious_snacks {
-    // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    // 修复：为导入的常量指定别名，匹配main中调用的名称
+    use self::fruits::PEAR as fruit;
+    use self::veggies::CUCUMBER as veggie;
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
