93cd7674ee7eb7a5e6e7abb34979dbf390bd156e
CODINGKINGWHM <13683191287@163.COM>
2025-12-15 14:13:55 +0000

    xxx

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -3,13 +3,14 @@
 // Execute `rustlings hint clippy2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    for x in option {
+    
+    // 优化点：用 if let 替代 for 循环处理 Option，更语义化
+    if let Some(x) = option {
         res += x;
     }
+    
     println!("{}", res);
 }
