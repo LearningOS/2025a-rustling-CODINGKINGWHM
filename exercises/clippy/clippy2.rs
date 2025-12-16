616ec9b23ac4410d35309d2bb6815e50d57552e6
CODINGKINGWHM <13683191287@163.COM>
2025-12-16 02:31:22 +0000

    xxx

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -1,9 +1,10 @@
 fn main() {
     let mut res = 42;
     let option = Some(12);
-
-    // 替代 match，简洁且无冗余
-    if let Some(val) = option {
-        res += val;
+    // TODO: Fix the Clippy lint.
+    for x in option {
+        res += x;
     }
+
+    println!("{res}");
 }
